624f027a6e4665fee41c1c8553f7f13e3e340ab4
Kalyan235 <106921696+Kalyan235@users.noreply.github.com>
2022-12-13 18:24:40 +0530

    Commited

---
+++ 00Fundamentals/src/com/java8/Employee.java
@@ -0,0 +1,51 @@
+package com.java8;
+
+public class Employee {
+
+	public int id;
+	public String name;
+	public double Salary;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		Salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return Salary;
+	}
+
+	public void setSalary(double salary) {
+		Salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", Salary=" + Salary + "]";
+	}
+	
+	
+	
+}
---
+++ 00Fundamentals/src/com/java8/GetSalary.java
@@ -0,0 +1,42 @@
+package com.java8;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+import java.util.stream.Collectors;
+
+public class GetSalary {
+
+	public static void main(String[] args) {
+
+		List<Employee> emp = new ArrayList<>();
+
+		emp.add(new Employee(1, "Kalyan", 35000));
+		emp.add(new Employee(2, "Mickyy", 35000));
+		emp.add(new Employee(3, "Aman", 95000));
+		emp.add(new Employee(4, "Raaji", 45000));
+
+		// ----> Print the employees who's salary is less than 95000;
+		 emp.stream().filter(e -> e.getSalary() < 5000).forEach(System.out::println);
+
+		// ----> Print Only Id's.
+		List<Integer> list = emp.stream().map(Employee::getId).collect(Collectors.toList());
+		System.out.println(list);
+
+		// ----> Print Only Names's.
+		List<String> names = emp.stream().map(Employee::getName).collect(Collectors.toList());
+
+		System.out.println(names);
+
+		List<Double> salary = emp.stream().map(Employee::getSalary).collect(Collectors.toList());
+
+		System.out.println(salary);
+		
+
+		// ----> Print Only Salary's.
+	
+	}
+
+}
